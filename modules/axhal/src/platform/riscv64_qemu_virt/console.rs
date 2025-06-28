bd5b8d2f9984ee9407a48225e64d78e2f93ac648
Asakura Mizu <asakuramizu111@gmail.com>
2025-06-28 20:25:28 +0800

    fix(hal): riscv console

--- modules/axhal/src/platform/riscv64_qemu_virt/console.rs
+++ modules/axhal/src/platform/riscv64_qemu_virt/console.rs
@@ -25,21 +25,15 @@ fn try_write_bytes(bytes: &[u8]) -> usize {
 
 /// Writes bytes to the console from input u8 slice.
 pub fn write_bytes(bytes: &[u8]) {
-    // If the address is from userspace, we need to copy the bytes to kernel space.
-    #[cfg(feature = "uspace")]
-    if bytes.as_ptr() as usize & (1 << 63) == 0 {
-        // Check if the address is valid.
-        let kernel_bytes = bytes.to_vec();
-        let mut write_len = 0;
-        while write_len < kernel_bytes.len() {
-            let len = try_write_bytes(&kernel_bytes[write_len..]);
-            if len == 0 {
-                break;
-            }
-            write_len += len;
-        }
-        return;
-    }
+    let bytes = if cfg!(feature = "uspace")
+        && (bytes.as_ptr() as usize) < axconfig::plat::PHYS_VIRT_OFFSET
+    {
+        // If the address is from userspace, we need to copy the bytes to kernel space.
+        &bytes.to_vec()
+    } else {
+        bytes
+    };
+
     let mut write_len = 0;
     while write_len < bytes.len() {
         let len = try_write_bytes(&bytes[write_len..]);
@@ -50,13 +44,25 @@ pub fn write_bytes(bytes: &[u8]) {
     }
 }
 
-/// Reads bytes from the console into the given mutable slice.
-/// Returns the number of bytes read.
-pub fn read_bytes(bytes: &mut [u8]) -> usize {
+fn read_bytes_impl(bytes: &mut [u8]) -> usize {
     sbi_rt::console_read(sbi_rt::Physical::new(
         bytes.len().min(MAX_RW_SIZE),
         virt_to_phys(VirtAddr::from_mut_ptr_of(bytes.as_mut_ptr())).as_usize(),
         0,
     ))
     .value
+}
+
+/// Reads bytes from the console into the given mutable slice.
+/// Returns the number of bytes read.
+pub fn read_bytes(bytes: &mut [u8]) -> usize {
+    if cfg!(feature = "uspace") && (bytes.as_mut_ptr() as usize) < axconfig::plat::PHYS_VIRT_OFFSET
+    {
+        let mut temp = bytes.to_vec();
+        let len = read_bytes_impl(&mut temp);
+        bytes[..len].copy_from_slice(&temp[..len]);
+        len
+    } else {
+        read_bytes_impl(bytes)
+    }
 }
