8e475d4d78f9954d3ac49ee3f8dca1e801681992
mivik <mivikq@gmail.com>
2025-07-12 00:16:50 +0800

    refactor: axfs-ng
    Co-authored-by: Asakura Mizu <asakuramizu111@gmail.com>

--- api/arceos_posix_api/src/imp/fs.rs
+++ api/arceos_posix_api/src/imp/fs.rs
@@ -2,19 +2,36 @@ use alloc::sync::Arc;
 use core::ffi::{c_char, c_int};
 
 use axerrno::{LinuxError, LinuxResult};
-use axfs::fops::OpenOptions;
-use axio::{PollState, SeekFrom};
-use axsync::Mutex;
+use axfs_ng::{FS_CONTEXT, FsContext, OpenOptions, OpenResult};
+use axfs_ng_vfs::{DirEntry, Location, Metadata};
+use axio::{PollState, Read, Seek, SeekFrom};
+use axsync::{Mutex, RawMutex};
 
 use super::fd_ops::{FileLike, get_file_like};
 use crate::{ctypes, utils::char_ptr_to_str};
 
+pub const AT_FDCWD: c_int = -100;
+
+pub fn with_fs<R>(
+    dirfd: c_int,
+    f: impl FnOnce(&mut FsContext<RawMutex>) -> LinuxResult<R>,
+) -> LinuxResult<R> {
+    let mut fs = FS_CONTEXT.lock();
+    if dirfd == AT_FDCWD {
+        f(&mut fs)
+    } else {
+        let dir = Directory::from_fd(dirfd)?.inner.clone();
+        f(&mut fs.with_current_dir(dir)?)
+    }
+}
+
+/// File wrapper for `axfs::fops::File`.
 pub struct File {
-    inner: Mutex<axfs::fops::File>,
+    inner: Mutex<axfs_ng::File<RawMutex>>,
 }
 
 impl File {
-    fn new(inner: axfs::fops::File) -> Self {
+    fn new(inner: axfs_ng::File<RawMutex>) -> Self {
         Self {
             inner: Mutex::new(inner),
         }
@@ -30,6 +47,18 @@ impl File {
             .downcast::<Self>()
             .map_err(|_| LinuxError::EINVAL)
     }
+
+    /// Get the inner node of the file.
+    pub fn inner(&self) -> &Mutex<axfs_ng::File<RawMutex>> {
+        &self.inner
+    }
+}
+
+fn duration_to_timespec(duration: core::time::Duration) -> ctypes::timespec {
+    ctypes::timespec {
+        tv_sec: duration.as_secs() as i64,
+        tv_nsec: duration.subsec_nanos() as i64,
+    }
 }
 
 impl FileLike for File {
@@ -42,21 +71,7 @@ impl FileLike for File {
     }
 
     fn stat(&self) -> LinuxResult<ctypes::stat> {
-        let metadata = self.inner.lock().get_attr()?;
-        let ty = metadata.file_type() as u8;
-        let perm = metadata.perm().bits() as u32;
-        let st_mode = ((ty as u32) << 12) | perm;
-        Ok(ctypes::stat {
-            st_ino: 1,
-            st_nlink: 1,
-            st_mode,
-            st_uid: 1000,
-            st_gid: 1000,
-            st_size: metadata.size() as _,
-            st_blocks: metadata.blocks() as _,
-            st_blksize: 512,
-            ..Default::default()
-        })
+        Ok(metadata_to_stat(&self.inner.lock().metadata()?))
     }
 
     fn into_any(self: Arc<Self>) -> Arc<dyn core::any::Any + Send + Sync> {
@@ -82,10 +97,7 @@ fn flags_to_options(flags: c_int, _mode: ctypes::mode_t) -> OpenOptions {
     match flags & 0b11 {
         ctypes::O_RDONLY => options.read(true),
         ctypes::O_WRONLY => options.write(true),
-        _ => {
-            options.read(true);
-            options.write(true);
-        }
+        _ => options.read(true).write(true),
     };
     if flags & ctypes::O_APPEND != 0 {
         options.append(true);
@@ -97,9 +109,51 @@ fn flags_to_options(flags: c_int, _mode: ctypes::mode_t) -> OpenOptions {
         options.create(true);
     }
     if flags & ctypes::O_EXEC != 0 {
+        options.execute(true);
+    }
+    if flags & ctypes::O_EXCL != 0 {
         options.create_new(true);
     }
+    if flags & ctypes::O_DIRECTORY != 0 {
+        options.directory(true);
+    }
     options
+}
+
+fn add_to_fd(result: OpenResult<RawMutex>) -> LinuxResult<c_int> {
+    match result {
+        OpenResult::File(file) => {
+            let file = File::new(file);
+            let fd = file.add_to_fd_table()?;
+            Ok(fd)
+        }
+        OpenResult::Dir(dir) => {
+            let dir = Directory::new(dir);
+            let fd = dir.add_to_fd_table()?;
+            Ok(fd)
+        }
+    }
+}
+
+pub fn metadata_to_stat(metadata: &Metadata) -> ctypes::stat {
+    let ty = metadata.node_type as u8;
+    let perm = metadata.mode.bits() as u32;
+    let st_mode = ((ty as u32) << 12) | perm;
+    ctypes::stat {
+        st_dev: metadata.device,
+        st_ino: metadata.inode,
+        st_mode,
+        st_nlink: metadata.nlink as _,
+        st_uid: metadata.uid,
+        st_gid: metadata.gid,
+        st_rdev: 0,
+        st_size: metadata.size as _,
+        st_blksize: metadata.block_size as _,
+        st_blocks: metadata.blocks as _,
+        st_atime: duration_to_timespec(metadata.atime),
+        st_mtime: duration_to_timespec(metadata.mtime),
+        st_ctime: duration_to_timespec(metadata.ctime),
+    }
 }
 
 /// Open a file by `filename` and insert it into the file descriptor table.
@@ -109,10 +163,37 @@ fn flags_to_options(flags: c_int, _mode: ctypes::mode_t) -> OpenOptions {
 pub fn sys_open(filename: *const c_char, flags: c_int, mode: ctypes::mode_t) -> c_int {
     let filename = char_ptr_to_str(filename);
     debug!("sys_open <= {:?} {:#o} {:#o}", filename, flags, mode);
-    syscall_body!(sys_open, {
+    let options = flags_to_options(flags, mode);
+    filename
+        .and_then(|filename| {
+            options
+                .open(&*FS_CONTEXT.lock(), filename)
+                .map_err(Into::into)
+        })
+        .and_then(add_to_fd)
+        .unwrap_or_else(|e| -(e as i32))
+}
+
+/// Open or create a file.
+/// fd: file descriptor
+/// filename: file path to be opened or created
+/// flags: open flags
+/// mode: see man 7 inode
+/// return new file descriptor if succeed, or return -1.
+pub fn sys_openat(dirfd: c_int, name: *const c_char, flags: c_int, mode: ctypes::mode_t) -> c_int {
+    let name = match char_ptr_to_str(name) {
+        Ok(s) => s,
+        Err(_) => return -1,
+    };
+
+    debug!(
+        "sys_openat <= {} {:?} {:#o} {:#o}",
+        dirfd, name, flags, mode
+    );
+
+    syscall_body!(sys_openat, {
         let options = flags_to_options(flags, mode);
-        let file = axfs::fops::File::open(filename?, &options)?;
-        File::new(file).add_to_fd_table()
+        with_fs(dirfd, |fs| Ok(options.open(fs, name)?)).and_then(add_to_fd)
     })
 }
 
@@ -145,7 +226,10 @@ pub unsafe fn sys_stat(path: *const c_char, buf: *mut ctypes::stat) -> c_int {
         }
         let mut options = OpenOptions::new();
         options.read(true);
-        let file = axfs::fops::File::open(path?, &options)?;
+        let file = OpenOptions::new()
+            .read(true)
+            .open(&*FS_CONTEXT.lock(), path?)?
+            .into_file()?;
         let st = File::new(file).stat()?;
         unsafe { *buf = st };
         Ok(0)
@@ -191,7 +275,7 @@ pub fn sys_getcwd(buf: *mut c_char, size: usize) -> *mut c_char {
             return Ok(core::ptr::null::<c_char>() as _);
         }
         let dst = unsafe { core::slice::from_raw_parts_mut(buf as *mut u8, size as _) };
-        let cwd = axfs::api::current_dir()?;
+        let cwd = FS_CONTEXT.lock().current_dir().absolute_path()?;
         let cwd = cwd.as_bytes();
         if cwd.len() < size {
             dst[..cwd.len()].copy_from_slice(cwd);
@@ -212,7 +296,67 @@ pub fn sys_rename(old: *const c_char, new: *const c_char) -> c_int {
         let old_path = char_ptr_to_str(old)?;
         let new_path = char_ptr_to_str(new)?;
         debug!("sys_rename <= old: {:?}, new: {:?}", old_path, new_path);
-        axfs::api::rename(old_path, new_path)?;
+        FS_CONTEXT.lock().rename(old_path, new_path)?;
         Ok(0)
     })
+}
+
+/// Directory wrapper for `axfs::fops::Directory`.
+pub struct Directory {
+    inner: Location<RawMutex>,
+    pub offset: Mutex<u64>,
+}
+
+impl Directory {
+    fn new(inner: Location<RawMutex>) -> Self {
+        Self {
+            inner,
+            offset: Mutex::new(0),
+        }
+    }
+
+    pub fn inner(&self) -> &Location<RawMutex> {
+        &self.inner
+    }
+
+    fn add_to_fd_table(self) -> LinuxResult<c_int> {
+        super::fd_ops::add_file_like(Arc::new(self))
+    }
+
+    /// Open a directory by `fd`.
+    pub fn from_fd(fd: c_int) -> LinuxResult<Arc<Self>> {
+        let f = super::fd_ops::get_file_like(fd)?;
+        f.into_any()
+            .downcast::<Self>()
+            .map_err(|_| LinuxError::EINVAL)
+    }
+}
+
+impl FileLike for Directory {
+    fn read(&self, _buf: &mut [u8]) -> LinuxResult<usize> {
+        Err(LinuxError::EBADF)
+    }
+
+    fn write(&self, _buf: &[u8]) -> LinuxResult<usize> {
+        Err(LinuxError::EBADF)
+    }
+
+    fn stat(&self) -> LinuxResult<ctypes::stat> {
+        Err(LinuxError::EBADF)
+    }
+
+    fn into_any(self: Arc<Self>) -> Arc<dyn core::any::Any + Send + Sync> {
+        self
+    }
+
+    fn poll(&self) -> LinuxResult<PollState> {
+        Ok(PollState {
+            readable: true,
+            writable: false,
+        })
+    }
+
+    fn set_nonblocking(&self, _nonblocking: bool) -> LinuxResult {
+        Ok(())
+    }
 }
